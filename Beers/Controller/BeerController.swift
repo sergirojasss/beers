90249d07d1d58b3a9752a37a0aef49778cfa25df
Sergi Rojas <sergirojasserra@gmail.com>
2020-02-19 17:28:54 +0100

    Getting ready the structure

---
+++ Beers/Controller/BeerController.swift
@@ -0,0 +1,12 @@
+//
+//  BeerController.swift
+//  Beers
+//
+//  Created by Sergi Rojas on 19/02/2020.
+//  Copyright © 2020 Sergi Rojas. All rights reserved.
+//
+
+import Foundation
+
+
+// Relation between Entity Beer & DB
